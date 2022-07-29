bc94e52a8181c64170dce81bc09bc213cef3547f
saimir djoumoi <saiiimiiir@MateBook.localdomain>
2022-07-29 14:04:19 +0200

    export csv et option + new build 1.0

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# STB_marion
+version 1.0
+
+Build:
+env GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build main.go
+
+Start:
+./main
+or
+
+pm2 start ecosystem_gpark.config.js
--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"bufio"
+	"encoding/csv"
 	"fmt"
 	"log"
 	"os"
 	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -19,7 +21,7 @@ type MapFromFile struct {
 	nb_stb                    int
 	imei_stb                  []string
 	adresse_ip                string
-	option                    string
+	options                   []string
 }
 
 // varible environement pour le nom du fichier a traiter
@@ -29,7 +31,7 @@ var FilePath = os.Getenv("file_path")
 var OutputFileName = os.Getenv("output_file_name")
 
 // header du fichier csv de sortie
-//header := ["SRVID","Date d\'ouverture co","Date de souscription","Techno","Reseau","Code INSEE","Nb STB","STB1","STB2","STB3","STB4","STB5","STB6","STB7","STB8","STB9","STB10","Adresse IP","OPTION"]
+
 // initialise les variables avec les variables env
 func Assign_env() {
 	//fichier par défaut , fichier de test
@@ -83,36 +85,40 @@ func arrayToMap(fileLines [][]string) []MapFromFile {
 	//Tableau de MapFromFIle qui contiendra les lignes a insérer dans le fichier CSV de sortie
 	var arrayOfMap []MapFromFile
 	// defintion d'une ligne dans le fichier CSV de sortie
-	var mapToArrayOfMap MapFromFile
+	var mapOfLine MapFromFile
 	var line = 0
 	for line < len(fileLines) {
 		if line == 0 {
-			mapToArrayOfMap.srvid = fileLines[line][0]
-			mapToArrayOfMap.techno = fileLines[line][3]
-			mapToArrayOfMap.reseau = fileLines[line][4]
-			mapToArrayOfMap.imei_stb, mapToArrayOfMap.nb_stb = numAndImeiStb(fileLines[line])
-			mapToArrayOfMap.date_souscription = fileLines[line][2]
-			mapToArrayOfMap.date_ouverture_commerical = fileLines[line][1]
-			mapToArrayOfMap.code_insee = fileLines[line][6]
-			mapToArrayOfMap.adresse_ip = getIP(fileLines[line])
-			mapToArrayOfMap.option = "MULTI"
-			arrayOfMap = append(arrayOfMap, mapToArrayOfMap)
+			mapOfLine.srvid = fileLines[line][0]
+			mapOfLine.techno = fileLines[line][3]
+			mapOfLine.reseau = fileLines[line][4]
+			mapOfLine.imei_stb, mapOfLine.nb_stb = numAndImeiStb(fileLines[line])
+			mapOfLine.date_souscription = fileLines[line][2]
+			mapOfLine.date_ouverture_commerical = fileLines[line][1]
+			mapOfLine.code_insee = fileLines[line][6]
+			mapOfLine.adresse_ip = getIP(fileLines[line])
+			mapOfLine.options = append(mapOfLine.options, getOptions(fileLines[line]))
 			fmt.Println("ligne : %s", line+1)
 			line++
 		} else if line > 0 {
 			if fileLines[line][0] == fileLines[line-1][0] {
+				mapOfLine.options = append(mapOfLine.options, getOptions(fileLines[line]))
+				if line+1 >= len(fileLines) {
+					arrayOfMap = append(arrayOfMap, mapOfLine)
+				}
 				line++
 			} else {
-				mapToArrayOfMap.srvid = fileLines[line][0]
-				mapToArrayOfMap.techno = fileLines[line][3]
-				mapToArrayOfMap.reseau = fileLines[line][4]
-				mapToArrayOfMap.imei_stb, mapToArrayOfMap.nb_stb = numAndImeiStb(fileLines[line])
-				mapToArrayOfMap.date_souscription = fileLines[line][2]
-				mapToArrayOfMap.date_ouverture_commerical = fileLines[line][1]
-				mapToArrayOfMap.code_insee = fileLines[line][6]
-				mapToArrayOfMap.adresse_ip = getIP(fileLines[line])
-				mapToArrayOfMap.option = "MULTI"
-				arrayOfMap = append(arrayOfMap, mapToArrayOfMap)
+				arrayOfMap = append(arrayOfMap, mapOfLine)
+				mapOfLine.options = nil
+				mapOfLine.srvid = fileLines[line][0]
+				mapOfLine.techno = fileLines[line][3]
+				mapOfLine.reseau = fileLines[line][4]
+				mapOfLine.imei_stb, mapOfLine.nb_stb = numAndImeiStb(fileLines[line])
+				mapOfLine.date_souscription = fileLines[line][2]
+				mapOfLine.date_ouverture_commerical = fileLines[line][1]
+				mapOfLine.code_insee = fileLines[line][6]
+				mapOfLine.adresse_ip = getIP(fileLines[line])
+				mapOfLine.options = append(mapOfLine.options, getOptions(fileLines[line]))
 				fmt.Println("ligne : %s", line+1)
 				line++
 			}
@@ -138,6 +144,18 @@ func numAndImeiStb(line []string) ([]string, int) {
 	return listImei, nbImei
 }
 
+// via une expression reguliére , on récupere l'option de la ligne actuelle
+func getOptions(line []string) string {
+	var option string
+	var re = regexp.MustCompile(`^[A-Z_]*$`)
+	for i := 4; i < len(line); i++ {
+		if re.FindString(line[i]) != "" && line[i] != "BYT" && line[i] != "FT" || line[i] == "VOD_24" {
+			option = line[i]
+		}
+	}
+	return option
+}
+
 // retourne l'adresse IP a aprtir de la ligne de fichier lue actuellement
 func getIP(line []string) string {
 	var re = regexp.MustCompile(`(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}`)
@@ -150,13 +168,84 @@ func getIP(line []string) string {
 	return ip
 }
 
+//Trie les données du tableau à deux dimensions
+func sortData(arrayOfMap []MapFromFile) [][]string {
+	var dataToCSV [][]string
+	header := []string{"SRVID", "Date d'ouverture co", "Date de souscription", "Techno", "Reseau", "Code INSEE", "Nb STB", "STB1", "STB2", "STB3", "STB4", "STB5", "STB6", "STB7", "STB8", "STB9", "STB10", "Adresse IP", "OPTIONS"}
+	//ajout des titres des colonnes
+	dataToCSV = append(dataToCSV, header)
+	for i := 0; i < len(arrayOfMap); i++ {
+		var line []string
+		line = append(line, arrayOfMap[i].srvid)
+		line = append(line, arrayOfMap[i].date_ouverture_commerical)
+		line = append(line, arrayOfMap[i].date_souscription)
+		line = append(line, arrayOfMap[i].techno)
+		line = append(line, arrayOfMap[i].reseau)
+		line = append(line, arrayOfMap[i].code_insee)
+		line = append(line, strconv.FormatInt(int64(arrayOfMap[i].nb_stb), 10))
+		var cursor = 7
+		for index := 0; index <= 9; index++ {
+
+			if index < len(arrayOfMap[i].imei_stb) {
+				line = append(line, arrayOfMap[i].imei_stb[index])
+			} else {
+				line = append(line, "")
+			}
+			cursor++
+		}
+
+		line = append(line, arrayOfMap[i].adresse_ip)
+		var strOption = ""
+		for index, value := range arrayOfMap[i].options {
+			if index == 0 {
+				strOption = strOption + value
+			} else {
+				strOption = strOption + " | " + value
+			}
+		}
+		line = append(line, strOption)
+		dataToCSV = append(dataToCSV, line)
+	}
+	return dataToCSV
+}
+
+// Creation d'un fichier CSV a partir d'un tableau a 2 dimensions et un header
+func writeCSV(dataToCSV [][]string, outputFileName string) {
+	csvFile, err := os.Create(outputFileName)
+
+	if err != nil {
+		log.Fatalf("failed creating file: %s", err)
+	}
+
+	csvwriter := csv.NewWriter(csvFile)
+	csvwriter.Comma = ';'
+
+	for _, empRow := range dataToCSV {
+		_ = csvwriter.Write(empRow)
+	}
+	csvwriter.Flush()
+	csvFile.Close()
+}
+
 func main() {
 	// configuration depuis les variables environement du fichier ecosystem.config.js
 	Assign_env()
 	// lecture du fichier
+	fmt.Println("lecture du fichier")
 	fileLines := ReadFile("./assets/SMV_test_fichier.csv")
+	fmt.Println("reception de l'array de Map ")
 	arrayOfMapFromFile := arrayToMap(fileLines)
 	for _, value := range arrayOfMapFromFile {
+		fmt.Println("")
 		fmt.Println(value)
 	}
+	fmt.Println("---------------------------------------------")
+	fmt.Println("trie des données ")
+	fmt.Println("---------------------------------------------")
+	dataToCSVOut := sortData(arrayOfMapFromFile)
+	for _, value := range dataToCSVOut {
+		fmt.Println("")
+		fmt.Println(value)
+	}
+	writeCSV(dataToCSVOut, OutputFileName)
 }
